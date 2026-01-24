29cf1ffa6b6e03898bca66e2341670df9fc119a2
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-01-24 22:48:44 +0530

    Update FigureList.jsx

--- myapp/src/components/FigureList.jsx
+++ myapp/src/components/FigureList.jsx
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import BasicFigures from "./BasicFigures";
 import "../FigureList.css"; 
 
-
 function FigureList () {
     
     const[figures, setFigures] = useState([
