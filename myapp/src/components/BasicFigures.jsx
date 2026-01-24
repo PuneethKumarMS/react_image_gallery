108df8fdd30be19065249860d8199a11103c6a99
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-01-24 22:49:10 +0530

    Update BasicFigures.jsx

--- myapp/src/components/BasicFigures.jsx
+++ myapp/src/components/BasicFigures.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import "../BasicFigure.css";
 
 function BasicFigures({imageUrl, caption, onDelete}) {
-
     return(
         <div className="figure-container">
             <img src="{imageUrl}" alt={caption} className="figure-image" />
             <p className="figure-caption">{caption}</p>
             <button className="delete-btn" onClick={onDelete}>Remove</button>
-
         </div>
     );
 }
-
 export default BasicFigures;
